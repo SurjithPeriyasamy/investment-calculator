b791fc9bd8f9b565ef2159ab34de04eaa2bb816f
Surjith <mahindhiran005@gmail.com>
2025-06-29 14:43:58 +0530

    header added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class AppComponent {
-  protected title = 'investment-calculator';
+  protected title = 'Investment Calculator';
 }
---
+++ src/app/header/header.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class HeaderComponent {
+  @Input({ required: true }) headTitle?: string;
+}
